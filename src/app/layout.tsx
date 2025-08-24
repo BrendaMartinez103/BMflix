7f0200d0aea52e695e2ff77133967ecaf16ced2e
BrendaMartinez103 <brenh_97@hotmail.com>
2025-08-24 16:35:45 -0300

    fix lay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,11 +39,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <head />
-      <body>
-        <OffcanvasNavbar brandName="BMflix" brandHref="/" />
-        {children}
-      </body>
+      <head>
+        <link rel="icon" href={`/favicon.ico?${V}`} sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href={`/favicon-32x32.png?${V}`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`/favicon-16x16.png?${V}`} />
+        <link rel="apple-touch-icon" href={`/apple-touch-icon.png?${V}`} />
+      </head>
+      <body>{children}</body>
     </html>
   )
 }
+
